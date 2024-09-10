5794552902a62fcaba8287bfb2ddc015ad312b84
Haris <harisvengattiri@gmail.com>
2024-09-10 20:15:44 +0530

    fix: removed unwanted files

---
+++ cdn/pdf/delivery_order.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../../config.php";
+
+$order = $_GET["id"];
+
+$html = file_get_contents(BASEURL.'/prints/order_pdf?id='.$order);
+require_once('vendor/autoload.php');
+
+$mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8','format' => 'A4','margin_left' => 10,'margin_right' => 10,'margin_top' => 30,'margin_bottom' => 10,'margin_header' => 0,'margin_footer' => 0,]);
+
+$mpdf->setAutoTopMargin = 'stretch';
+$mpdf->showWatermarkImage = true;
+$mpdf->setAutoBottomMargin = 'stretch';
+$mpdf->SetDisplayMode('fullpage');
+$mpdf->WriteHTML($html);
+$mpdf->Output('Delivery Order #'.$order.'.pdf', 'I');
+exit;
+?>
