3577b0b94ccaf8b718baf673e5785374c692c519
Haris <harisvengattiri@gmail.com>
2024-11-01 00:37:16 +0530

    fix: added items with jw number for understanding the order

--- loads/get_orders.php
+++ loads/get_orders.php
@@ -4,10 +4,21 @@ require_once "../config.php";
 if ($_POST['c_id']) {
 
     $customer = $_POST['c_id'];
-    $sql = "SELECT * FROM `sales_order` WHERE `customer`='$customer' AND `flag`=0";
+    $sql = "SELECT id,jw FROM `sales_order` WHERE `customer`='$customer' AND `flag`=0";
     $query = mysqli_query($conn,$sql);
     while($result = mysqli_fetch_array($query)) {
-        echo "<option value='{$result['jw']}'>JW|{$result['jw']}</option>";
+        $so = $result['id'];
+        $jw = $result['jw'];
+        $sql1 = "SELECT itm.name AS itemName FROM order_item soi INNER JOIN items itm ON soi.item=itm.id
+                 WHERE soi.order_id=$so";
+        $query1 = mysqli_query($conn,$sql1);
+        $itemNames = [];
+        while($result1 = mysqli_fetch_array($query1)) {
+            $itemSubName = substr($result1['itemName'], 0, 7);
+            $itemNames[] = $itemSubName;
+        }
+        $itemNamesString = implode(", ", $itemNames);
+        echo "<option value='{$jw}'>JW|{$jw} - [{$itemNamesString}]</option>";
     }
 } else {
     echo '<option>Order Not Found</option>';
